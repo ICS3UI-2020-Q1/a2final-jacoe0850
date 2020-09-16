964e58d608ec8ea8a4f47d32618ac95ea36be75b
jacoe0850 <jacoe0850@wrdsb.ca>
2020-09-16 12:48:15 +0000

    best guess

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Will calculate cost of pizza based of diameter
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("What is the size of the pizza (in cm):");
+    double diam = input.nextDouble();
+    double labour = 0.75;
+    double power = 0.99;
+    double ingredients = diam*0.50;
+    double subtotal = labour+power+ingredients;
+    double tax = subtotal*.13;
+    double total = subtotal+tax;
+    if (diam <= 19.99){
+      System.out.println("Subtotal: $" + subtotal + 
+      " Taxes: $" + tax + 
+      " Total: $" + total);
+      } else if (diam >= 20 && diam <=39.99){
+      System.out.println("Subtotal: $" + subtotal + 
+      " Taxes: $" + tax + 
+      " Total: $" + total);
+      } else if (diam > 40){
+      System.out.println("Subtotal: $" + subtotal +  
+      "Taxes: $" + tax + 
+      "Total: $" + total);
+
   }
+}
 }
