69d785a129aa8ce5894cbcf4621496d6351edeeb
jacoe0850 <jacoe0850@wrdsb.ca>
2020-09-16 12:56:59 +0000

    Final Pizza

--- Main.java
+++ Main.java
@@ -11,26 +11,35 @@ public class Main {
    */
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+    // Asking for the diameter of the pizza
     System.out.println("What is the size of the pizza (in cm):");
+    // Creating variable for the diameter
     double diam = input.nextDouble();
+    // variables for the two costs that never change
     double labour = 0.75;
     double power = 0.99;
+    // variables for all the numbers witgh calculations
     double ingredients = diam*0.50;
     double subtotal = labour+power+ingredients;
     double tax = subtotal*.13;
     double total = subtotal+tax;
+    // if statements telling all the costs and correct final message
     if (diam <= 19.99){
-      System.out.println("Subtotal: $" + subtotal + 
-      " Taxes: $" + tax + 
-      " Total: $" + total);
+        System.out.println("Subtotal: $" + subtotal);
+        System.out.println("Taxes: $" + tax);
+        System.out.println("Total: $" + total);
+        System.out.println("We are going to make you a cute little pizza!");
       } else if (diam >= 20 && diam <=39.99){
-      System.out.println("Subtotal: $" + subtotal + 
-      " Taxes: $" + tax + 
-      " Total: $" + total);
+        System.out.println("Subtotal: $" + subtotal);
+        System.out.println("Taxes: $" + tax);
+        System.out.println("Total: $" + total);
+        System.out.println("This will be delicious!");
       } else if (diam > 40){
-      System.out.println("Subtotal: $" + subtotal +  
-      "Taxes: $" + tax + 
-      "Total: $" + total);
+        System.out.println("Subtotal: $" + subtotal);
+        System.out.println("Taxes: $" + tax);
+        System.out.println("Total: $" + total);
+        System.out.println("Whoa, big pizza! You might need a truck to get this home!");
+
 
   }
 }
